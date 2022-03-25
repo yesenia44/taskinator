c54f25fb246ceb05b28f65f2a00dd95a6d2739a9
Yesenia <yeseniavsqz@gmail.com>
2022-03-24 20:48:53 -0700

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
@@ -8,4 +8,16 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); 
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+listItemEl.textContent = "This is a new task.";
+tasksToDoEl.appendChild(listItemEl);
+console.log(event);
+};
+formEl.addEventListener("submit", createTaskHandler);
